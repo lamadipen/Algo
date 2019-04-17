49bcde556ef03e6bba4db986349a108cb67ab67e
lamadipen <lamadipen@yahoo.com>
2019-04-16 22:10:19 -0500

    Add SHA256 Hashing  solution Add JavaMD5 Hashing solution

---
+++ CrackingCode/MasterAlgorithm/src/main/java/com/dp/hrank/JavaMD5.java
@@ -0,0 +1,22 @@
+package com.dp.hrank;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class JavaMD5 {
+
+    public static void main(String... args) throws NoSuchAlgorithmException {
+        String str = "Hello";
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+
+        md.update(str.getBytes());
+        byte[] digest = md.digest();
+
+        StringBuilder sb = new StringBuilder();
+        for (byte b : digest) {
+            sb.append(String.format("%02x", b));
+        }
+        System.out.println(sb.toString());
+    }
+}
