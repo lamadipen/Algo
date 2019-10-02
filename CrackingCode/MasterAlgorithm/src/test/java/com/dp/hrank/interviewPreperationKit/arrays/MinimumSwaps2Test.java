22161e4a7779940eaa271e5e4a99d5271a65e45d
lamadipen <lamadipen@yahoo.com>
2019-10-01 21:47:29 -0700

    Adds all the file

---
+++ CrackingCode/MasterAlgorithm/src/test/java/com/dp/hrank/interviewPreperationKit/arrays/MinimumSwaps2Test.java
@@ -0,0 +1,42 @@
+package com.dp.hrank.interviewPreperationKit.arrays;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MinimumSwaps2Test {
+
+    @Test
+    public void minimumSwapsTest() {
+        int [] arr = new int[]{7,1,3,2,4,5,6} ;
+        int [] oneElement = new int[]{7} ;
+        int [] twoElement = new int[]{7,1} ;
+        int [] threeElement = new int[]{2,7,1} ;
+
+        assertEquals(0, minimumSwaps(oneElement));;
+        assertEquals(1, minimumSwaps(twoElement));;
+        assertEquals(2, minimumSwaps(threeElement));;
+    }
+
+    private int minimumSwaps(int[] arr) {
+        if(arr.length == 1)
+            return 0;
+        mergeSort(arr,0,arr.length-1);
+        return 1;
+    }
+
+    private void mergeSort(int[] arr, int start, int end) {
+        if(start < end) {
+            int mid = (start+end)/2;
+            mergeSort(arr,start,mid);
+            mergeSort(arr,mid+1,end);
+            merge(arr, start, mid, end);
+        }
+    }
+
+    private void merge(int[] arr, int start, int mid, int end) {
+        //https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/tutorial/
+    }
+
+
+}
