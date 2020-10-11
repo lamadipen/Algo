2c9170c1a56c7daa6e14c33e08f2d80e71047af2
lamadipen <lamadipen@yahoo.com>
2020-10-11 12:04:38 -0700

    Demo to Deepa

--- CrackingCode/MasterAlgorithm/src/test/java/kata/sortingandsearching/SearchInRotatedArrayTest.java
+++ CrackingCode/MasterAlgorithm/src/test/java/kata/sortingandsearching/SearchInRotatedArrayTest.java
@@ -7,13 +7,14 @@ public class SearchInRotatedArrayTest {
 
     @Test
     public void searchTest() {
+        int expected = 8;
         SearchInRotatedArray searchInRotatedArray = new SearchInRotatedArray();
 
         int[] arr = new int[]{15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14};
 
         int actual = searchInRotatedArray.find(arr, 5);
 
-        Assert.assertEquals(8, actual);
+        Assert.assertEquals(expected, actual);
     }
 
 }
