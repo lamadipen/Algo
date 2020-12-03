2d392d2d20a1f5dcbd59d1762ced9c0163109835
lamadipen <lamadipen@yahoo.com>
2020-12-03 14:03:35 -0800

    PatternMatchingSolution

---
+++ CrackingCode/MasterAlgorithm/src/test/java/kata/moderate/PatternMatchingSolutionTest.java
@@ -0,0 +1,25 @@
+package kata.moderate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PatternMatchingSolutionTest {
+
+    @Test
+    public void doesMatchesTest() {
+        PatternMatchingSolution patternMatchingSolution = new PatternMatchingSolution();
+
+        boolean actual = patternMatchingSolution.doesMatches("aabab", "catcatgocatgo");
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void doesMatchesFirstOptimizationTest() {
+        PatternMatchingSolution patternMatchingSolution = new PatternMatchingSolution();
+
+        boolean actual = patternMatchingSolution.doesMatchesFirstOptimization("aabab", "catcatgocatgo");
+
+        Assert.assertTrue(actual);
+    }
+}
