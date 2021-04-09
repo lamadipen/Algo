534397575c48b054c43576b22d558d73c11b6110
lamadipen <lamadipen@yahoo.com>
2021-04-09 10:30:19 -0700

    Kforce InterviewSolution 2021 April 8

---
+++ CrackingCode/MasterAlgorithm/src/main/java/kforce/InterviewSolution.java
@@ -0,0 +1,42 @@
+package kforce;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Interview 2021 April 8
+ */
+public class InterviewSolution {
+
+    int binaryPatternMatching(String pattern, String s) {
+
+        Map<Character, Integer> vowels = new HashMap<>();
+        vowels.put('a', 0);
+        vowels.put('e', 0);
+        vowels.put('i', 0);
+        vowels.put('o', 0);
+        vowels.put('u', 0);
+        vowels.put('y', 0);
+
+        int patternLength = pattern.length();
+        int matchCount = 0;
+        for (int i = 0; i < s.length() - patternLength + 1; i++) {
+            String substring = s.substring(i, i + patternLength);
+            char[] chars = substring.toCharArray();
+            String result = "";
+            for (char c : chars) {
+                result += vowels.getOrDefault(c, 1);
+            }
+            if (result.equals(pattern)) {
+                matchCount++;
+            }
+        }
+
+        return matchCount;
+    }
+
+    public void readCsvFile() {
+
+
+    }
+}
---
+++ CrackingCode/MasterAlgorithm/src/test/java/kforce/InterviewSolutionTest.java
@@ -0,0 +1,21 @@
+package kforce;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InterviewSolutionTest {
+
+    @Test
+    public void binaryPatternMatchingTest1() {
+        InterviewSolution interviewSolution = new InterviewSolution();
+        int amazing = interviewSolution.binaryPatternMatching("010", "amazing");
+        Assert.assertEquals(2, amazing);
+    }
+
+    @Test
+    public void binaryPatternMatchingTest2() {
+        InterviewSolution interviewSolution = new InterviewSolution();
+        int amazing = interviewSolution.binaryPatternMatching("00", "aaaaaaaaaa");
+        Assert.assertEquals(9, amazing);
+    }
+}
