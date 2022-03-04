1c362033fc237f928a45374a5bf0610f741191d7
lamadipen <lamadipen@yahoo.com>
2022-03-04 10:44:56 -0600

    Min Max Riddle Stack

---
+++ CrackingCode/MasterAlgorithm/src/main/java/hackerrank/interviewprepkit/stacksandqueues/MinMaxRiddle.java
@@ -0,0 +1,69 @@
+package hackerrank.interviewprepkit.stacksandqueues;
+
+
+import java.util.Stack;
+
+/**
+ * Interview Preparation Kit | Stacks and Queues | Min Max Riddle
+ */
+public class MinMaxRiddle {
+
+    // Complete the riddle function below.
+    static long[] riddle(long[] input) {
+        // complete this function
+        Stack<Integer> stack = new Stack<>();
+
+        int noOfElement = input.length;
+
+        int[] leftBoundaries = new int[noOfElement];
+        int[] rightBoundaries = new int[noOfElement];
+
+        for (int index = 0; index < noOfElement; index++) {
+            leftBoundaries[index] = -1;
+            rightBoundaries[index] = noOfElement;
+        }
+
+        /** what is the index of the first element on my left smaller than me */
+        for (int index = 0; index < noOfElement; index++) {
+            while (!stack.isEmpty() && input[stack.peek()] >= input[index]) {
+                stack.pop();
+            }
+
+            if (!stack.isEmpty()) {
+                leftBoundaries[index] = stack.peek();
+            }
+            stack.add(index);
+        }
+
+        while (!stack.isEmpty()) {
+            stack.pop();
+        }
+
+        /** what is the index of the first element on my right smaller than me */
+        for (int index = noOfElement - 1; index >= 0; index--) {
+            while (!stack.isEmpty() && input[stack.peek()] >= input[index]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                rightBoundaries[index] = stack.peek();
+            }
+            stack.add(index);
+        }
+
+        long[] results = new long[noOfElement];
+
+        for (int index = 0; index < noOfElement; index++) {
+            results[index] = 0;
+        }
+
+        for (int index = 0; index < noOfElement; index++) {
+            int windowSize = rightBoundaries[index] - leftBoundaries[index] - 2;
+            results[windowSize] = Math.max(input[index], results[windowSize]);
+        }
+
+        for (int index = noOfElement - 2; index >= 0; index--) {
+            results[index] = Math.max(results[index], results[index + 1]);
+        }
+        return results;
+    }
+}
---
+++ CrackingCode/MasterAlgorithm/src/test/java/hackerrank/interviewprepkit/stacksandqueues/MinMaxRiddleTest.java
@@ -0,0 +1,43 @@
+package hackerrank.interviewprepkit.stacksandqueues;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class MinMaxRiddleTest {
+
+    /**
+     * https://www.youtube.com/watch?v=MJzZgHFc00U not that good but can be reference
+     * Approaching to the soultion
+     * Window size 1
+     * {2}, {6}, {1}, {12} => Max 12
+     * <p>
+     * Window size 2
+     * {2,6}, {6,1}, {1,12} => min {2, 1, 1} => max 2
+     * <p>
+     * window size 3
+     * {2,6,1} ,{6,1,12}  => min {1,1} => max 1
+     * <p>
+     * window size 4
+     * {2,6,1,12} => min 1  => max 1
+     * <p>
+     * Final Answer
+     * {12, 2 , 1, ,1}
+     * <p>
+     * steps while calculating the window size and max min
+     * input 	[2, 6, 1,12]
+     * left 	[-1,0,-1, 2]  what is the index of the first element on my left smaller than me use stack to navigate and maintain array
+     * right 	[2, 2, 4, 4]  what is the index of the first element on my right smaller than me  use stack to navigate and maintain array
+     * WSize    1, 0, 3 ,0  right[index] - left[index] -2
+     * result  [0, 0, 0, 0] initilize result array
+     * result  [12, 2, 1, 0] loop through input & apply result[WSize] = Max of input[index] or result[WSize]
+     * result  [12, 2, 1, 0] (length of Input - 2)reverse loop Max of result[index] or result[index +1]
+     */
+    @Test
+    public void riddle() {
+        long[] arr = new long[]{2, 6, 1, 12};
+        long[] actual = MinMaxRiddle.riddle(arr);
+        assertArrayEquals(new long[]{12, 2, 1, 1}, actual);
+    }
+
+}
