e3199696f725a522bb9a675337a0d518b0f09d9e
lamadipen <lamadipen@yahoo.com>
2022-03-04 13:16:59 -0600

    Min Max Riddle adding additional understanding

--- CrackingCode/MasterAlgorithm/src/test/java/hackerrank/interviewprepkit/stacksandqueues/MinMaxRiddleTest.java
+++ CrackingCode/MasterAlgorithm/src/test/java/hackerrank/interviewprepkit/stacksandqueues/MinMaxRiddleTest.java
@@ -28,7 +28,7 @@ public class MinMaxRiddleTest {
      * input 	[2, 6, 1,12]
      * left 	[-1,0,-1, 2]  what is the index of the first element on my left smaller than me use stack to navigate and maintain array
      * right 	[2, 2, 4, 4]  what is the index of the first element on my right smaller than me  use stack to navigate and maintain array
-     * WSize    1, 0, 3 ,0  right[index] - left[index] -2
+     * WSize    1, 0, 3 ,0  right[index] - left[index] -2 (we need -2 for 2 reasons, -1 as we are taking input length on right which will cause index out of bound error and -1 to adjust the result (index) in the array)
      * result  [0, 0, 0, 0] initilize result array
      * result  [12, 2, 1, 0] loop through input & apply result[WSize] = Max of input[index] or result[WSize]
      * result  [12, 2, 1, 0] (length of Input - 2)reverse loop Max of result[index] or result[index +1]
