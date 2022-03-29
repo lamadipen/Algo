952484e27b6d31b9c706e42e0670fdf8076c082b
lamadipen <lamadipen@yahoo.com>
2022-03-29 15:02:10 -0500

    MaxArraySumSolution Question

---
+++ CrackingCode/MasterAlgorithm/src/main/java/hackerrank/interviewprepkit/dynamicprogramming/MaxArraySumSolution.java
@@ -0,0 +1,12 @@
+package hackerrank.interviewprepkit.dynamicprogramming;
+
+
+/**
+ * Interview Preparation Kit | Dynamic Programming | Max Array Sum
+ */
+public class MaxArraySumSolution {
+    static int maxSubsetSum(int[] arr) {
+
+        return 0;
+    }
+}
---
+++ CrackingCode/MasterAlgorithm/src/test/java/hackerrank/interviewprepkit/dynamicprogramming/MaxArraySumSolutionTest.java
@@ -0,0 +1,11 @@
+package hackerrank.interviewprepkit.dynamicprogramming;
+
+import org.junit.Test;
+
+public class MaxArraySumSolutionTest {
+
+    @Test
+    public void maxSubsetSumTest() {
+        
+    }
+}
