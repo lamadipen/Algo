ed3460feec080500a037f305813b0b78cbc7c36c
lamadipen <lamadipen@yahoo.com>
2022-04-06 23:39:32 -0500

    MaxArraySumSolution

--- CrackingCode/MasterAlgorithm/src/main/java/hackerrank/interviewprepkit/dynamicprogramming/MaxArraySumSolution.java
+++ CrackingCode/MasterAlgorithm/src/main/java/hackerrank/interviewprepkit/dynamicprogramming/MaxArraySumSolution.java
@@ -6,7 +6,14 @@ package hackerrank.interviewprepkit.dynamicprogramming;
  */
 public class MaxArraySumSolution {
     static int maxSubsetSum(int[] arr) {
+        if (arr.length == 0) return 0;
+        arr[0] = Math.max(0, arr[0]);
+        if (arr.length == 1) return arr[0];
+        arr[1] = Math.max(arr[0], arr[1]);
 
-        return 0;
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = Math.max(arr[i - 1], arr[i - 2] + arr[i]);
+        }
+        return arr[arr.length - 1];
     }
 }
--- CrackingCode/MasterAlgorithm/src/test/java/hackerrank/interviewprepkit/dynamicprogramming/MaxArraySumSolutionTest.java
+++ CrackingCode/MasterAlgorithm/src/test/java/hackerrank/interviewprepkit/dynamicprogramming/MaxArraySumSolutionTest.java
@@ -1,11 +1,28 @@
 package hackerrank.interviewprepkit.dynamicprogramming;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MaxArraySumSolutionTest {
 
     @Test
-    public void maxSubsetSumTest() {
-        
+    public void maxSubsetSumTest1() {
+        int[] arr = {3, 7, 4, 6, 5};
+        int actual = MaxArraySumSolution.maxSubsetSum(arr);
+        Assert.assertEquals(13, actual);
+    }
+
+    @Test
+    public void maxSubsetSumTest2() {
+        int[] arr = {2, 1, 5, 8, 4};
+        int actual = MaxArraySumSolution.maxSubsetSum(arr);
+        Assert.assertEquals(11, actual);
+    }
+
+    @Test
+    public void maxSubsetSumTest3() {
+        int[] arr = {3, 5, -7, 8, 10};
+        int actual = MaxArraySumSolution.maxSubsetSum(arr);
+        Assert.assertEquals(15, actual);
     }
 }
